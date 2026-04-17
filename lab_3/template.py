9c9223a681ea95cb8d1bbfce5bd92f421d75b131
Deniz Yalim <denizyalimyilmaz@gmail.com>
2026-04-17 18:11:13 +0200

    finished chromosome stuff

--- lab_3/template.py
+++ lab_3/template.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 def min_max_norm(val, min_val, max_val, new_min, new_max):
@@ -7,22 +8,47 @@ def min_max_norm(val, min_val, max_val, new_min, new_max):
 
 
 class Chromosome:
-    def __init__(self, length, array=None):  # if array is None, initialize with a random binary vector
-        pass
+
+    def __init__(self, length, array: list = None):  # if array is None, initialize with a random binary vector
+        if array is None:
+            array = np.random.randint(2, size=length)
+
+        self.genes = array
+        self.length = length
 
     def decode(self, lower_bound, upper_bound, aoi):
+        # Decode the chromosome segment from bit lower_bound to bit upper_bound into a real number within range aoi. You may use the provided min_max_norm helper.
+        segment = self.genes[lower_bound:upper_bound]
+        int_val = 0
+        for i, j in enumerate(segment):  # LITTLE OR BIG ENDIAN ?
+            int_val += j * 2**i
+
+        k = upper_bound - lower_bound
+        max_val = 2**k - 1
+
+        decoded = min_max_norm(int_val, 0, max_val, aoi[0], aoi[1])
+        return decoded
+
+    def mutation(self, probability):  # flip each bit with the given probability
+        for i, bit in enumerate(self.genes):
+            rando = random.randint(0, 10)
+            if rando < probability * 10:  # hit
+                self.genes[i] = 1 - bit
         pass
 
-    def mutation(self, probability):
-        pass
+    def crossover(self, other):  # Perform one-point crossover with another chromosome. Return two offspring.
+        genes1 = self.genes
+        genes2 = other.genes
+        point = random.randint(1, self.length - 1)
+        new_genes1 = np.concatenate((genes1[:point], genes2[point:]))
+        new_genes2 = np.concatenate((genes2[:point], genes1[point:]))
+        return Chromosome(self.length, new_genes1), Chromosome(self.length, new_genes2)
 
-    def crossover(self, other):
-        pass
 
-
-# TODO: implement your group's objective function here
 def objective_function(*args):
-    pass
+    # f ( x 1 , x 2 ) = 20 + x 1 2 − 10 cos ⁡ ( 2 π x 1 ) + x 2 2 − 10 cos ⁡ ( 2 π x 2 )
+    x1, x2 = args
+    return 20 + x1**2 - 10 * np.cos(2 * np.pi * x1) + x2**2 - 10 * np.cos(2 * np.pi * x2)
 
 
 class GeneticAlgorithm:
