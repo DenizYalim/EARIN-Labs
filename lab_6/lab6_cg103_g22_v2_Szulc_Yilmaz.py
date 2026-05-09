328dd56fb5379482366c3bf2f8566774722d410a
Deniz Yalim <denizyalimyilmaz@gmail.com>
2026-05-09 13:15:11 +0200

    actions are chosen from qtable + exploration

--- lab_6/lab6_cg103_g22_v2_Szulc_Yilmaz.py
+++ lab_6/lab6_cg103_g22_v2_Szulc_Yilmaz.py
@@ -6,7 +6,6 @@
 # truncated: hit 200 step limit
 # alpha: learning rate gamma: caring about future rewards
 
-
 import gymnasium as gym
 import numpy as np
 
@@ -17,17 +16,33 @@ state_low = env.observation_space.low
 state_high = env.observation_space.high
 num_actions = env.action_space.n
 
-q_table = np.zeros((*num_bins, num_actions))
+try:
+    q_table = np.load("q_table.npy")
+    print("Q-table loaded from file.")
+except FileNotFoundError:
+    q_table = np.zeros((*num_bins, num_actions))
 
 alpha = 0.1
 gamma = 0.99
 
+epsilon = 1.0  # how randomly action is taken at first
+epsilon_decay = 0.995
+epsilon_min = 0.05  # we always wwant some randomness
 
-def discretize_state(state):  #
+episodes = 5000  # epoch count
+
+
+def discretize_state(state):
     ratios = (state - state_low) / (state_high - state_low)
     ratios = np.clip(ratios, 0, 1)
     bins = (ratios * (num_bins - 1)).astype(int)
     return tuple(bins)
+
+
+def choose_action(discrete_state):
+    if np.random.random() < epsilon:
+        return env.action_space.sample()
+    return np.argmax(q_table[discrete_state])
 
 
 def update_q_table(discrete_state, action, reward, next_discrete_state):
@@ -37,49 +52,35 @@ def update_q_table(discrete_state, action, reward, next_discrete_state):
     q_table[discrete_state][action] = new_value
 
 
-state, info = env.reset()
-discrete_state = discretize_state(state)
+for episode in range(episodes):
+    state, info = env.reset()
+    discrete_state = discretize_state(state)
 
-total_reward = 0
-steps = 0
+    total_reward = 0
+    steps = 0
 
-terminated = False
-truncated = False
+    terminated = False
+    truncated = False
 
-time_stamp = 0
+    while not terminated and not truncated:
+        action = choose_action(discrete_state)
 
+        next_state, reward, terminated, truncated, info = env.step(action)
+        next_discrete_state = discretize_state(next_state)
 
-while not terminated and not truncated:  # one episode
-    time_stamp += 1
+        update_q_table(discrete_state, action, reward, next_discrete_state)
 
-    action = env.action_space.sample()
+        total_reward += reward
+        steps += 1
 
-    next_state, reward, terminated, truncated, info = env.step(action)
-    next_discrete_state = discretize_state(next_state)
+        state = next_state
+        discrete_state = next_discrete_state
 
-    update_q_table(discrete_state, action, reward, next_discrete_state)
+    epsilon = max(epsilon_min, epsilon * epsilon_decay)
 
-    total_reward += reward
-    steps += 1
+    if episode % 100 == 0:
+        print("episode:", episode, "total_reward:", total_reward, "steps:", steps, "epsilon:", round(epsilon, 4))
 
-    if time_stamp % 10 == 0:
-        print(f"Time stamp: {time_stamp}")
-        print("state:", np.round(state, 4))
-        print("discrete_state:", discrete_state)
-        print("action:", action)
-        print("next_state:", np.round(next_state, 4))
-        print("next_discrete_state:", next_discrete_state)
-        print("reward:", reward)
-        print("q-values:", np.round(q_table[discrete_state], 4), "\n")
-
-    state = next_state
-    discrete_state = next_discrete_state
-
-print("total_reward:", total_reward)
-print("steps:", steps)
-print("terminated:", terminated)
-print("truncated:", truncated)
-
-np.save("q_table.npy", q_table)  # SAVE Q-TABLE TO FILE
+np.save("q_table.npy", q_table)
 
 env.close()
