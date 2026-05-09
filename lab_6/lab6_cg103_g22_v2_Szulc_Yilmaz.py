02149bdbdc9e72c0dbec2ab4b3194fcf3f4aaef5
Deniz Yalim <denizyalimyilmaz@gmail.com>
2026-05-09 13:09:47 +0200

    random walk is now saved to the qtable and saved as file

--- lab_6/lab6_cg103_g22_v2_Szulc_Yilmaz.py
+++ lab_6/lab6_cg103_g22_v2_Szulc_Yilmaz.py
@@ -19,12 +19,22 @@ num_actions = env.action_space.n
 
 q_table = np.zeros((*num_bins, num_actions))
 
+alpha = 0.1
+gamma = 0.99
 
-def discretize_state(state):  # binning position and speed into discrete values
+
+def discretize_state(state):  #
     ratios = (state - state_low) / (state_high - state_low)
     ratios = np.clip(ratios, 0, 1)
     bins = (ratios * (num_bins - 1)).astype(int)
     return tuple(bins)
+
+
+def update_q_table(discrete_state, action, reward, next_discrete_state):
+    old_value = q_table[discrete_state][action]
+    next_max = np.max(q_table[next_discrete_state])
+    new_value = old_value + alpha * (reward + gamma * next_max - old_value)
+    q_table[discrete_state][action] = new_value
 
 
 state, info = env.reset()
@@ -37,15 +47,21 @@ terminated = False
 truncated = False
 
 time_stamp = 0
-while not terminated and not truncated:
+
+
+while not terminated and not truncated:  # one episode
     time_stamp += 1
+
     action = env.action_space.sample()
 
     next_state, reward, terminated, truncated, info = env.step(action)
     next_discrete_state = discretize_state(next_state)
 
+    update_q_table(discrete_state, action, reward, next_discrete_state)
+
     total_reward += reward
     steps += 1
+
     if time_stamp % 10 == 0:
         print(f"Time stamp: {time_stamp}")
         print("state:", np.round(state, 4))
@@ -53,14 +69,17 @@ while not terminated and not truncated:
         print("action:", action)
         print("next_state:", np.round(next_state, 4))
         print("next_discrete_state:", next_discrete_state)
-        print("reward:", reward, "\n")
+        print("reward:", reward)
+        print("q-values:", np.round(q_table[discrete_state], 4), "\n")
 
     state = next_state
     discrete_state = next_discrete_state
 
 print("total_reward:", total_reward)
 print("steps:", steps)
-print("terminated:", terminated)  # reacehed flag
-print("truncated:", truncated)  # run out of time
+print("terminated:", terminated)
+print("truncated:", truncated)
+
+np.save("q_table.npy", q_table)  # SAVE Q-TABLE TO FILE
 
 env.close()
